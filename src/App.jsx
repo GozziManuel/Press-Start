a4a5fbfdd59d71bed68ed035290305c74767cc67
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-03-27 11:00:11 +0100

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import ProductsListPage from "./pages/ProductsListPage";
+import DefaultTemplate from "./layouts/DefaultTemplate";
 
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
