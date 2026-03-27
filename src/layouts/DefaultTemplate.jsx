973331a937bd230cd4f608391d40e5a430e87d19
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-03-27 11:08:15 +0100

    Fix nav

--- src/layouts/DefaultTemplate.jsx
+++ src/layouts/DefaultTemplate.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router";
-import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export default function DefaultTemplate() {
   return (
     <div className="d-flex flex-column min-vh-100">
-      <Header />
+      <Navbar />
       <main className="flex-grow-1">
         <Outlet />
       </main>
