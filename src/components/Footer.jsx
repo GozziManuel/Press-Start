b4602635da2e144a01545d87939da135537bcc4b
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-03-29 00:15:52 +0100

    add footer, refresh css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,13 @@
+import "../assets/css/footer.css";
+
 export default function Footer() {
   return (
-    <>
-      <hr className="separator" />
-      <div className="byte-bounce gr-viola">
-        <p style={{ fontSize: "40px" }}>Footer</p>
+    <div className="footer ">
+      <div className="byte-bounce gr-viola ">
+        <p style={{ fontSize: "40px" }} className="m-0">
+          Footer
+        </p>
       </div>
-    </>
+    </div>
   );
 }
