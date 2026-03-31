779215fcece0f4cf7bd5e1e7c55e6136e4924525
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-03-31 10:43:51 +0200

    card

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,17 +1,25 @@
 import { Link } from "react-router";
+import "../assets/css/games.css";
 
-export default function GameCard({data}) {
+export default function GameCard({ data }) {
   return (
     <div className="card h-100">
-      <img src="img" className="card-img-top" alt="img" style={{minHeight:"100px"}}></img>
+      <img
+        src="img"
+        className="card-img-top"
+        alt="img"
+        style={{ minHeight: "100px" }}
+      ></img>
       <div className="card-body">
-        <h5 className="card-title" style={{fontSize:"50px"}}>{data.name}</h5>
+        <h5 className="card-title" style={{ fontSize: "50px" }} id="card-title">
+          {data.name}
+        </h5>
         <p className="card-text">
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </p>
-        <Link to={"/"} className="btn btn-primary floating">
-          Go somewhere
+        <Link to={"/"} className="button primary" id="btnCard">
+          See the Product
         </Link>
       </div>
     </div>
