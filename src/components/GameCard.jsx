96352d2350d7224da7bd8a361fccc1b4bbc6754c
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-04-01 18:44:50 +0200

    euros font

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -12,13 +12,16 @@ export default function GameCard({ data }) {
           style={{ height: "35vh" }}
         ></img>
         <div className="card-body">
-          <h5 className="card-title" style={{ fontSize: "30px" }}>
+          <h5 className="card-title" style={{ fontSize: "25px" }}>
             {data.name}
           </h5>
-          <hr className="my-0" />
+          <hr
+            className="my-0 "
+            style={{ border: "solid 2px var(--light-blue)" }}
+          />
           <p style={{ fontSize: "30px" }}>
             {data.price} {""}
-            <span style={{ fontFamily: "star-crush" }}>&euro;</span>
+            <span style={{ fontFamily: "pixel-sans" }}>&euro;</span>
           </p>
         </div>
       </div>
