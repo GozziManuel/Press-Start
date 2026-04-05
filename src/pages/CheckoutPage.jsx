071f87b3eb9b650788ad92f8b0daf53de23775b7
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-04-05 18:14:42 +0200

    placeholder

--- src/pages/CheckoutPage.jsx
+++ src/pages/CheckoutPage.jsx
@@ -183,14 +183,8 @@ export default function Checkout() {
               className="card text-bg-danger mb-3"
               style={{ maxWidth: "18rem;" }}
             >
-              <div className="card-header">Header</div>
-              <div className="card-body">
-                <h5 className="card-title">Danger card title</h5>
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card’s content.
-                </p>
-              </div>
+              <div className="card-header">Placeholder</div>
+              <div className="card-body"></div>
             </div>
           </>
         )}
