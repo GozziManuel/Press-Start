f42b9ce369d66e587c99ef7dc3dd889e324577a8
Gozzi Manuel <manuel2006gozzi@gmail.com>
2026-04-10 19:32:36 +0200

    fix bug

--- src/components/ColorsSplashCursor.jsx
+++ src/components/ColorsSplashCursor.jsx
@@ -219,7 +219,9 @@ function ColorsSplashCursor({
       gl.attachShader(program, vertexShader);
       gl.attachShader(program, fragmentShader);
       gl.linkProgram(program);
-      if (!gl.getProgramParameter(program, gl.LINK_STATUS)) return program;
+      if (!gl.getProgramParameter(program, gl.LINK_STATUS))
+        console.trace(gl.getProgramInfoLog(program));
+      return program;
     }
 
     function getUniforms(program) {
@@ -237,7 +239,9 @@ function ColorsSplashCursor({
       const shader = gl.createShader(type);
       gl.shaderSource(shader, source);
       gl.compileShader(shader);
-      if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) return shader;
+      if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+        console.trace(gl.getShaderInfoLog(shader));
+      return shader;
     }
 
     function addKeywords(source, keywords) {
