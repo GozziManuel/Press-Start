327411836a79f0913af25fd0e49a3eae679ff6f2
MhZ9191 <hegic91@gmail.com>
2026-04-10 20:25:14 +0200

    Add advanced filter complete

---
+++ src/assets/data/dataList.js
@@ -0,0 +1,32 @@
+export const selectOptions = [
+  { value: "all", nome: "All" },
+  { value: "name", nome: "Name" },
+  { value: "price", nome: "Price" },
+  { value: "discount_value", nome: "Discounted" },
+  { value: "created_at", nome: "Last" },
+];
+export const AdvancedConsoleOptions = [
+  { value: "NES", nome: "NES" },
+  { value: "SNES", nome: "SNES" },
+  { value: "Nintendo 64", nome: "Nintendo 64" },
+  { value: "Sega Mega Drive", nome: "Sega Mega Drive" },
+  { value: "PlayStation 1", nome: "PlayStation 1" },
+  { value: "Sega Saturn", nome: "Sega Saturn" },
+  { value: "Arcade", nome: "Arcade" },
+];
+export const AdvancedPublisherOptions = [
+  { value: "Nintendo", nome: "Nintendo" },
+  { value: "Sega", nome: "Sega" },
+  { value: "Sony", nome: "Sony" },
+  { value: "Vari", nome: "Vari" },
+];
+export const AdvancedGenreOptions = [
+  { value: "Shooter", nome: "Shooter" },
+  { value: "Survival Horror", nome: "Survival Horror" },
+  { value: "Racing", nome: "Racing" },
+  { value: "Puzzle", nome: "Puzzle" },
+  { value: "Fighting", nome: "Fighting" },
+  { value: "Action-Adventure", nome: "Action-Adventure" },
+  { value: "RPG", nome: "RPG" },
+  { value: "Platform", nome: "Platform" },
+];
